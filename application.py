ec231a753f35ea50fcc1229d8f4c64718e97b47c
Mark Kluber <145130822+mkluber@users.noreply.github.com>
2023-12-05 09:10:53 +0100

    .

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 application = Flask(__name__)
 api = Api(application)
-CORS(application, origins=["https://static.outworldindustries.com"])
+CORS(application, origins=["https://www.outworldindustries.com"])
 
 dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
 table = dynamodb.Table('Fortunes')
@@ -87,88 +87,4 @@ api.add_resource(UpdateFortune, '/updatefortune')
 api.add_resource(DeleteFortune, '/deletefortune')
 
 if __name__ == '__main__':
-    application.run(debug=True)
-
-
-# @application.route('/')
-# def index():
-#     return render_template('index.html')
-
-
-# @application.route("/addfortune/", methods=['POST'])
-# def addfortune():
-#     fortune = request.form['addfortune']
-#     origin = request.form['addorigin']
-#     table.put_item(
-#         Item={
-#         'FortuneName': fortune,
-#         'FortuneOrigin': origin
-#         }
-#     )
-#     return render_template('index.html')
-
-
-# @application.route("/scanfortune/", methods=['GET'])
-# def scanfortune():
-#     response = table.scan()
-#     items = response['Items']
-#     print(items)
-#     return render_template('index.html', fortunes=items)
-
-# @application.route("/readfortune/", methods=['GET', 'POST'])
-# def readfortune():
-#     fortune_key = request.form['readfortune']
-#     origin_key = request.form['readorigin']
-#     response = table.get_item(
-#         Key={
-#             'FortuneName': fortune_key,
-#             'FortuneOrigin': origin_key
-#         }
-#     )
-#     item = response['Item']
-#     print(item)
-#     return render_template('index.html', fortune=item)
-
-
-
-
-# @application.route("/updatefortune/", methods=['GET', 'POST'])
-# def updatefortune():
-#     fortune_key = request.form['updatefortune']
-#     origin_key = request.form['updateorigin']
-#     author = request.form['updateattribute1']
-#     color = request.form['updateattribute2']
-#     response = table.update_item(
-#         Key={
-#             'FortuneName': fortune_key,
-#             'FortuneOrigin': origin_key
-#         },
-#         AttributeUpdates={
-#             'FortuneAuthor': {
-#                 'Value'  : author,
-#                 'Action' : 'PUT' # available options -> DELETE(delete), PUT(set), ADD(increment)
-#             },
-#             'FortuneColor': {
-#                 'Value'  : color,
-#                 'Action' : 'PUT' # available options -> DELETE(delete), PUT(set), ADD(increment)
-#             }
-#         }
-#     )
-#     return render_template('index.html')
-
-
-# @application.route("/deletefortune/", methods=['GET', 'POST'])
-# def deletefortune():
-#     fortune_key = request.form['deletefortune']
-#     origin_key = request.form['deleteorigin']
-#     response = table.delete_item(
-#         Key={
-#             'FortuneName': fortune_key,
-#             'FortuneOrigin': origin_key
-#         }
-#     )
-#     return render_template('index.html')
-
-# if __name__ == "__main__":
-#         application.run()
-
+    application.run()
